a39f7b7df2f1a24e14e6db1a7fdc3498a509b6a7
n4ze3m <n4ze3m@gmail.com>
2023-09-13 19:44:14 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# Deno Shortner
+
+
+A simple URL shortner written in Deno. Application is using Deno KV store to store the shortned URL and redirect to the original URL.
+
+
+## Usage
+
+```bash
+deno run --allow-read --allow-net --unstable .\server.ts
+```
+
+This will start the server on port 8000.
+
+
+## API
+
+### POST /create
+
+Create a new short URL.
+
+```bash
+curl -X POST -H "Content-Type: application/json" -d '{"url":"https://www.google.com"}' http://localhost:8000/create
+```
+
+### GET /:id
+
+Redirect to the original URL.
+
+```bash
+curl -X GET http://localhost:8000/:id
+```
+
+## License
+
+MIT
---
+++ server.ts
@@ -0,0 +1,52 @@
+import { Application, Router } from "https://deno.land/x/oak@v12.6.1/mod.ts";
+import { nanoid } from "https://deno.land/x/nanoid@v3.0.0/mod.ts";
+
+const app = new Application();
+const router = new Router();
+const R_URL = new RegExp(/^(http|https):\/\/[^ "]+$/);
+const kv = await Deno.openKv();
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+router.post("/create", async (context) => {
+  const data = await context.request.body().value;
+
+  const request_url = context.request.url;
+
+  if (!data?.url) {
+    context.response.status = 400;
+    context.response.body = { error: "No URL found" };
+    return;
+  }
+
+  if (!R_URL.test(data.url)) {
+    context.response.status = 400;
+    context.response.body = { error: "Invalid URL" };
+    return;
+  }
+
+  const id = nanoid(7);
+
+  await kv.set(["short", id], data.url);
+  context.response.body = {
+    url: `${request_url.origin}/${id}`,
+  };
+});
+
+router.get("/:id", async (context) => {
+  const id = context.params.id;
+  const data = await kv.get(["short", id]);
+  if (!data.value) {
+    context.response.status = 404;
+    context.response.body = { error: "URL not found" };
+    return;
+  }
+
+  context.response.status = 301;
+  context.response.redirect(String(data.value));
+});
+
+console.log("Server running on http://localhost:8000");
+await app.listen({ port: 8000 });
+
